1462de531e3be5e45d83771d32052d4899476972
Arzu Kirici <arzukirici@gmail.com>
2025-11-25 14:18:38 -0500

    Simplify PortableText and disable TS checking

--- components/PortableText.tsx
+++ components/PortableText.tsx
@@ -1,14 +1,8 @@
 // @ts-nocheck
 "use client";
 
-
-
 import { PortableText as SanityPortableText } from "@portabletext/react";
 
-
-
 export default function PortableText({ value }: { value: any }) {
-
   return <SanityPortableText value={value} />;
-
 }
