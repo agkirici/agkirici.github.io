f0d761190818a7eaed1cba7abcdc612ea83bf34e
Arzu Kirici <arzukirici@gmail.com>
2025-11-26 11:35:54 -0500

    Add Sanity CDN to Next.js image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
   // Sanity Studio requires transpilation
   transpilePackages: ['next-sanity'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
